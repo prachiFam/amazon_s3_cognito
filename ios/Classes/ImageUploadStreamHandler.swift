8fc99e4943fb710d1d2e9766a9875e0816c05e1b
Prachi Shrivastava <prachi@famproperties.com>
2021-10-12 18:41:11 +0530

    added code.

--- ios/Classes/ImageUploadStreamHandler.swift
+++ ios/Classes/ImageUploadStreamHandler.swift
@@ -1,135 +1,48 @@
+//
+//  ImageUploadListener.swift
+//  amazon_s3_cognito
+//
+//  Created by Paras mac on 11/10/21.
+//
 import Flutter
-import UIKit
-import AWSS3
-import AWSCore
 
+class ImageUploadStreamHandler:NSObject, FlutterStreamHandler{
 
-public class SwiftAmazonS3CognitoPlugin: NSObject, FlutterPlugin {
+    private var eventSink: FlutterEventSink?
 
-private static let imageUploadEventChannel:String = "amazon_s3_cognito_images_upload_steam"
+    func addImageUploadResult(imageData:ImageData) {
 
-private static  var imageUploadStreamHandler = ImageUploadStreamHandler()
+        let data: [String: Any?] = [
+            "filePath" : imageData.filePath,
+            "fileName" : imageData.fileName,
+            "uniqueId" : imageData.uniqueId,
+            "isUploadError" : imageData.isUploadError,
+            "state" : imageData.state,
+            "amazonImageUrl" : imageData.amazonImageUrl ,
+            "progress" : imageData.progress,
+            "failureReason":imageData.failureReason,
+            "imageUploadFolder":imageData.imageUploadFolder]
 
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "amazon_s3_cognito", binaryMessenger: registrar.messenger())
-
-
-    FlutterEventChannel(name: imageUploadEventChannel, binaryMessenger: registrar.messenger())
-        .setStreamHandler(imageUploadStreamHandler)
-
-    let instance = SwiftAmazonS3CognitoPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-         if(call.method.elementsEqual("uploadImage")){
-              uploadImageForRegion(call,result: result)
-          }else if(call.method.elementsEqual("deleteImage")){
-              deleteImage(call,result: result)
-          }else if(call.method.elementsEqual("uploadImages")){
-            uploadMultipleImages(call,result: result)
+        if(self.eventSink != nil){
+            self.eventSink!(data)
         }
-      }
-
-    func uploadImageForRegion(_ call: FlutterMethodCall, result: @escaping FlutterResult){
-              let arguments = call.arguments as? NSDictionary
-              let imagePath = arguments!["filePath"] as? String
-              let bucket = arguments!["bucket"] as? String
-              let identity = arguments!["identity"] as? String
-              let fileName = arguments!["imageName"] as? String
-              let region = arguments!["region"] as? String
-              let subRegion = arguments!["subRegion"] as? String
-
-            let contentTypeParam = arguments!["contentType"] as? String
-
-
-              print("region" + region!)
-
-        let awsHelper:AwsImageUploadHelper = AwsImageUploadHelper.init()
-
-        awsHelper.uploadImageForRegion(imagePath: imagePath, bucket: bucket, identity: identity, fileName: fileName, region: region, subRegion: subRegion, contentTypeParam: contentTypeParam) { (awsUploadReult) in
-            result(awsUploadReult)}
 
     }
 
-    func uploadMultipleImages(_ call: FlutterMethodCall, result: @escaping FlutterResult)  {
+    func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
 
-        let arguments = call.arguments as? NSDictionary
-
-        let bucket = arguments!["bucket"] as? String
-        let identityPoolId = arguments!["identity"] as? String
-        let region = arguments!["region"] as? String
-        let subRegion = arguments!["subRegion"] as? String
-
-        let needFileProgressUpdateAlso = arguments!["needProgressUpdateAlso"] as? Bool
-
-        var needProgressUpdate:Bool = false
-        if(needFileProgressUpdateAlso != nil){
-            needProgressUpdate = needFileProgressUpdateAlso!
-        }
-
-        let imagesListString = arguments!["imageDataList"] as? String
-
-        if(imagesListString != nil){
-
-            let jsonData = imagesListString!.data(using: .utf8)!
-
-            var images:[ImageData] = []
-            if let json = try! JSONSerialization.jsonObject(with: jsonData, options: .allowFragments) as? [Any] {
-                for item in json {
-                    if let object = item as? [String: Any] {
-                        // ID
-
-                        let filePath = object["filePath"] as! String
-                        let fileName = object["fileName"] as! String
-                        let uniqueId = object["uniqueId"] as! String
-                        let contentType = object["contentType"] as? String
-
-                        let imageUploadFolder = object["imageUploadFolder"] as? String
-
-                        let imageDataInner:ImageData = ImageData(filePath: filePath, fileName: fileName, uniqueId: uniqueId, contentType: contentType,imageFolderInBucket: imageUploadFolder)
-                         images.append(imageDataInner)
-
-
-
-
-                    }
-                }
-
-            }
-
-
-            if(region == nil || subRegion == nil || bucket == nil || identityPoolId == nil  ){
-                result("function paramters are not supplied properly. Region, subregion, buckernamen identityPoolId cannot be null or empty")
-            }else{
-                let multiAwsUploadHelper:AwsMultiImageUploadHelper = AwsMultiImageUploadHelper.init(region: region!, subRegion: subRegion!, identity: identityPoolId!, bucketName: bucket!, needFileProgressUpdateAlso: needProgressUpdate)
-
-                multiAwsUploadHelper.uploadMultipleImages(imagesData:images, imageUploadSreamHelper:SwiftAmazonS3CognitoPlugin.imageUploadStreamHandler)
-            }
-        }else{
-            result("image list is empty")
-        }
-
-
+        self.eventSink = events
+        return nil
 
     }
 
-    func deleteImage(_ call: FlutterMethodCall, result: @escaping FlutterResult){
-        let arguments = call.arguments as? NSDictionary
-        let bucket = arguments!["bucket"] as? String
-        let identity = arguments!["identity"] as? String
-        let fileName = arguments!["imageName"] as? String
-        let region = arguments!["region"] as? String
-        let subRegion = arguments!["subRegion"] as? String
+    func onCancel(withArguments arguments: Any?) -> FlutterError? {
 
-        let awsHelper:AwsImageUploadHelper = AwsImageUploadHelper.init()
-
-        awsHelper.deleteImage(bucket: bucket, identity: identity, fileName: fileName, region: region, subRegion: subRegion) { deleteImageResult in
-            result(deleteImageResult)}
-
-
+        eventSink = nil
+        return nil
     }
+
 
 
 }
+
